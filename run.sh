a4dc50230c98457d3c72c8b1bb3af778c0743c83
Lucas Torri <lucastorri@gmail.com>
2012-01-04 14:34:05 -0200

    script to run multiple bots in different channels

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUT_DIR=/output/folder
+IRC_SERVER="irc.freenode.net"
+BOT_NAME="logbot"
+CHANNELS="scala scalatra playframework"
+
+for CHANNEL in $CHANNELS
+do
+	java -cp target/ircbot-assembly-1.0.jar co.torri.ircbot.IRCBot "$BOT_NAME-${CHANNEL:0:5}" $IRC_SERVER "#$CHANNEL" $OUT_DIR > "$CHANNEL.log" &
+done
